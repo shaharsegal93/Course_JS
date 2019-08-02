c115485aa480368f63d21f5f713df198c954a603
shaharse <shahars@perfectomobile.com>
2019-08-02 11:27:34 +0300

    Update Budget project

--- Budgety/app.js
+++ Budgety/app.js
@@ -8,8 +8,27 @@ var budgetController = (function() {
 
 
 var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
-    
+    return {
+        getinput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+                
+            };
+        },
+        
+        getDomstrings: function(){
+            return DOMstrings;
+        }
+    }; 
     
     
 }) ();
@@ -19,14 +38,18 @@ var UIController = (function() {
 
 var controller = (function (budgetCtrl, UICtrl) {
     
-    document.querySelector('.add__btn ').addEventListener('click', function() {
-        
-    });
+    var DOM = UICtrl.getDomstrings();
     
-    document.addEventListener('keypress', function(event) {
-        
-        console.log(event);
-        
+    var ctrlAddItem = function() {
+        var input = UICtrl.getinput();
+        console.log(input);
+    }
+    
+    document.querySelector(DOM.inputBtn).addEventListener('.click', ctrlAddItem);
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode == 13 || event.which == 13){
+            ctrlAddItem();    
+        }
     });
     
     
